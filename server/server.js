f5d94e543c51545cac55e1f8783cc8c72f2fe72a
generyand <generyan.dep@gmail.com>
2024-10-20 18:10:07 +0800

    addding address goods

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const adminProductsRouter = require("./routes/admin/products-routes");
 
 const shopProductsRouter = require("./routes/shop/products-routes");
 const shopCartRouter = require("./routes/shop/cart-routes");
-// const shopAddressRouter = require("./routes/shop/address-routes");
+const shopAddressRouter = require("./routes/shop/address-routes");
 // const shopOrderRouter = require("./routes/shop/order-routes");
 // const shopSearchRouter = require("./routes/shop/search-routes");
 // const shopReviewRouter = require("./routes/shop/review-routes");
@@ -53,7 +53,7 @@ app.use("/api/admin/products", adminProductsRouter);
 
 app.use("/api/shop/products", shopProductsRouter);
 app.use("/api/shop/cart", shopCartRouter);
-// app.use("/api/shop/address", shopAddressRouter);
+app.use("/api/shop/address", shopAddressRouter);
 // app.use("/api/shop/order", shopOrderRouter);
 // app.use("/api/shop/search", shopSearchRouter);
 // app.use("/api/shop/review", shopReviewRouter);
